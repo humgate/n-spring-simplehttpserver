0b734050799e3b1dcac7ef9fde728ae03f2da9f5
Pavel Yanovsky <humgate@gmail.com>
2022-01-31 09:30:09 +0300

    Закончил первую задачу ДЗ

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -10,15 +10,20 @@ import java.util.concurrent.Executors;
 
 public class Server {
     static final String PUBLIC_FOLDER = "public";
+    static final int THREAD_POOL_THREADS = 64;
+    static final short SERVER_PORT = 9999;
     static final List<String> validPaths = initServerPaths();
 
     public static void main(String[] args) {
-        final ExecutorService threadPool = Executors.newFixedThreadPool(64);
-        try (final var serverSocket = new ServerSocket(9999)) {
+        final ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_THREADS);
+        try (final var serverSocket = new ServerSocket(SERVER_PORT)) {
             while (!Thread.currentThread().isInterrupted()) {
                 try {
                     /* Закрытие сокета выносим отсюда в тред-обработчик из тредпула. Иначе запущенный тред
-                    в тредпуле даже не успеет начать обработку, а socket уже будет закрыт в этом треде */
+                    в тредпуле даже не успеет начать обработку, а socket уже будет закрыт в этом треде
+                    * Remove Socket closure from here to the threadPool thread, otherwise threadPool thread
+                    * will get closed socket
+                    */
                     final var socket = serverSocket.accept();
                     threadPool.execute(() -> processClientRequests(socket));
                 } catch (IOException e) {
@@ -32,12 +37,20 @@ public class Server {
         }
     }
 
+    /**
+     * Reads server resource paths from file system older (constant)
+     * @return pathsList
+     */
     private static List<String> initServerPaths() {
         List<String> validPaths = Arrays.asList((Objects.requireNonNull(new File(Server.PUBLIC_FOLDER).list())));
         validPaths.replaceAll(s -> "/"+ s);
         return validPaths;
     }
 
+    /**
+     * Processes client request and closes the socket
+     * @param socket - accepted client socket
+     */
     public static void processClientRequests(Socket socket) {
         try (final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              final var out = new BufferedOutputStream(socket.getOutputStream())) {
