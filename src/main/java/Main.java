d5a3f03a451c293d32912d1608d53d739f455e50
Pavel Yanovsky <humgate@gmail.com>
2022-02-03 23:06:34 +0300

    Реализована возможность задать handler для фолдера

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
@@ -10,37 +9,50 @@ public class Main {
 
     public static void main(String[] args) {
         final var server = new Server();
-        server.addHandler(Method.GET, "/spring.svg", (r, o) -> {
-            final Path filePath;
-            final String mimeType;
+
+        //Common "root" ("parent") handler for each resource located in PUBLIC_FOLDER
+        server.addHandler(Method.GET, "\\", (r, o) -> {
             try {
-                filePath = Path.of(".", PUBLIC_FOLDER, r.getPath());
-                mimeType = Files.probeContentType(filePath);
+                final Path filePath = Path.of(".", PUBLIC_FOLDER, r.getPath());
+                final String mimeType = Files.probeContentType(filePath);
                 final var length = Files.size(filePath);
-                o.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
+                o.write(Server.buildResponseStatusHeadersOnOK(mimeType, length).getBytes());
                 Files.copy(filePath, o);
                 o.flush();
             } catch (InvalidPathException | IOException e) {
+              e.printStackTrace();
                 //Not found. Respond with 404
                 try {
-                    o.write((
-                            "HTTP/1.1 404 Not Found\r\n" +
-                                    "Content-Length: 0\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
+                    o.write(Server.buildResponseStatusHeadersOnFail().getBytes());
                     o.flush();
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
             }
         });
+
+        //Custom exact match handler for PUBLIC_FOLDER/
+        server.addHandler(Method.GET, "\\", (r, o) -> {
+            try {
+                final Path filePath = Path.of(".", PUBLIC_FOLDER, r.getPath());
+                final String mimeType = Files.probeContentType(filePath);
+                final var length = Files.size(filePath);
+                o.write(Server.buildResponseStatusHeadersOnOK(mimeType, length).getBytes());
+                Files.copy(filePath, o);
+                o.flush();
+            } catch (InvalidPathException | IOException e) {
+                e.printStackTrace();
+                //Not found. Respond with 404
+                try {
+                    o.write(Server.buildResponseStatusHeadersOnFail().getBytes());
+                    o.flush();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+
         server.listen(SERVER_PORT);
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,28 +1,36 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    static final String PUBLIC_FOLDER = "public";
     static final int THREAD_POOL_THREADS = 64;
-    static final List<String> validPaths = initServerPaths();
+    //handlers collection
     static Map<Method,Map<String, Handler>> handlerMap = new HashMap<>();
 
+    /**
+     * Handler adder
+     * @param method - http method
+     * @param path - http request path. May be absolute path including resource name, or may be path to folder
+     *             resources folder
+     * @param handler - handler object
+     */
     public void addHandler(Method method, String path, Handler handler) {
         Map<String, Handler> innerMap = new HashMap<>();
         innerMap.put(path, handler);
         handlerMap.put(method, innerMap);
     }
 
+    /**
+     * Parses Request string to Request object. Read only request line for simplicity.
+     * String must be in form GET /path HTTP/1.1
+     * @param strRequest - input string
+     * @return - Request objects if parsed ok and null if not
+     */
     public static Request parseRequest (String strRequest) {
-        // read only request line for simplicity
-        // must be in form GET /path HTTP/1.1
         final var parts = strRequest.split(" ");
 
         if (parts.length != 3) {
@@ -47,16 +55,19 @@ public class Server {
         return new Request(method,path,null,new byte[0]);
     }
 
+    /**
+     * Initiate server listening operation for given port.
+     * Listening is cycle of waiting for new client connections and launching received request handling
+     * in new thread (in threadPool) upon connection
+     * @param port - server port
+     */
     public void listen(short port) {
         final ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_THREADS);
         try (final var serverSocket = new ServerSocket(port)) {
             while (!Thread.currentThread().isInterrupted()) {
                 try {
-                    /* Закрытие сокета выносим отсюда в тред-обработчик из тредпула. Иначе запущенный тред
-                    в тредпуле даже не успеет начать обработку, а socket уже будет закрыт в этом треде
-                    * Remove Socket closure from here to the threadPool thread, otherwise threadPool thread
-                    * will get closed socket
-                    */
+                   /* Remove Socket closure from here to the threadPool thread, otherwise threadPool thread
+                    * will get already closed socket */
                     final var socket = serverSocket.accept();
                     threadPool.execute(() -> processClientRequests(socket));
                 } catch (IOException e) {
@@ -71,19 +82,7 @@ public class Server {
     }
 
     /**
-     * Reads server resource paths from file system folder (constant)
-     *
-     * @return pathsList
-     */
-    private static List<String> initServerPaths() {
-        List<String> validPaths = Arrays.asList((Objects.requireNonNull(new File(Server.PUBLIC_FOLDER).list())));
-        validPaths.replaceAll(s -> "/" + s);
-        return validPaths;
-    }
-
-    /**
      * Processes client request and closes the socket
-     *
      * @param socket - accepted client socket
      */
     private static void processClientRequests(Socket socket) {
@@ -102,26 +101,58 @@ public class Server {
                 return;
             }
 
+            //find registered handler exactly matching handlers path
             Handler handler = handlerMap.get(request.getMethod()).get(request.getPath());
+            System.out.println("request path: " + request.getPath());
 
+            //if exact match not found, try to find handler by parent path to request path
             if (handler == null) {
-                //no handlers registered
-                out.write((
-                        "HTTP/1.1 404 Not Found\r\n" +
-                                "Content-Length: 0\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.flush();
-                socket.close();
-                return;
+                System.out.println("handler not found for exact request path");
+                Path parent = Path.of(request.getPath()).getParent();
+                System.out.println("parent path: " + parent);
+
+                //if this one fails either - no registered handlers found, respond with 404
+                handler = handlerMap.get(request.getMethod()).get(parent.toString());
+                if (handler == null) {
+                    System.out.println("handler not found for request parent path");
+                    out.write(buildResponseStatusHeadersOnFail().getBytes());
+                    out.flush();
+                    socket.close();
+                    return;
+                }
             }
 
+            //handler found, have it to process the request
             handler.handle(request,out);
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Builds response status line and headers for OK status
+     * @param mimeType - mimetype of response body
+     * @param length - respince body length
+     * @return - response status line and headers
+     */
+    public static String buildResponseStatusHeadersOnOK(String mimeType, long length) {
+        return "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: " + mimeType + "\r\n" +
+                "Content-Length: " + length + "\r\n" +
+                "Connection: close\r\n" +
+                "\r\n";
+    }
+
+    /**
+     * Builds response status line and headers for "Not found" status
+     * @return - response status line and headers
+     */
+    public static String buildResponseStatusHeadersOnFail() {
+        return  "HTTP/1.1 404 Not Found\r\n" +
+                "Content-Length: 0\r\n" +
+                "Connection: close\r\n" +
+                "\r\n";
     }
 }
 
